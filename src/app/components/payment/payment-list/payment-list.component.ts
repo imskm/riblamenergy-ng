57d4a870e1362d43bf65c7f00024cecb7cb21f0a
Shek Muktar <im.skm07@gmail.com>
2021-05-07 14:25:33 +0530

    list: payment: Integrated payment list API

--- src/app/components/payment/payment-list/payment-list.component.ts
+++ src/app/components/payment/payment-list/payment-list.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
 
+import { PaymentService } from '../../../services/payment.service';
+import { PaymentModel } from '../../../models/payment.model';
+
+@Injectable({
+	providedIn: 'root'
+})
 @Component({
   selector: 'app-payment-list',
   templateUrl: './payment-list.component.html',
   styleUrls: ['./payment-list.component.css']
 })
 export class PaymentListComponent implements OnInit {
+  
+  payments: Array<PaymentModel>;
 
-  constructor() { }
+  constructor(private paymentService: PaymentService) {
+  	this.payments = [];
+  }
 
   ngOnInit(): void {
+  	this.fetchPayments();
+  }
+
+  fetchPayments() {
+  	this.paymentService.fetch()
+  		.subscribe((res) => {
+  			this.payments = res.data;
+  		});
   }
 
 }
