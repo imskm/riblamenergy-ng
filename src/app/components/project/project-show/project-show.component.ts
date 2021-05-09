5d11f07693ba87a7251eefdaa80ddee1f3ee973d
Shek Muktar <im.skm07@gmail.com>
2021-05-10 02:52:05 +0530

    show: project: Integrated project show API

--- src/app/components/project/project-show/project-show.component.ts
+++ src/app/components/project/project-show/project-show.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Injectable } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
+import { ProjectService } from '../../../services/project.service';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,14 +14,25 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 export class ProjectShowComponent implements OnInit {
 
   project_id = null;
+  project = null;
+  payments = [];
 
   constructor(
   	private route: ActivatedRoute,
-  	private router: Router
+  	private router: Router,
+    private projectSerivce: ProjectService
   ) { }
 
   ngOnInit(): void {
   	this.project_id = this.route.snapshot.paramMap.get("id");
+    this.fetchProject();
+  }
+
+  fetchProject() {
+    this.projectSerivce.get(this.project_id).subscribe((res: any) => {
+      this.project = res.data.project;
+      this.payments = res.data.payments;
+    });
   }
 
 }
