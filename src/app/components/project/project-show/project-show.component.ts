ed29b3f1d13d119a19966aa02f70bcd26eba76eb
Shek Muktar <im.skm07@gmail.com>
2021-05-10 00:14:42 +0530

    show: porject: Added query params and added html, was blank before

--- src/app/components/project/project-show/project-show.component.ts
+++ src/app/components/project/project-show/project-show.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
+@Injectable({
+  providedIn: 'root'
+})
 @Component({
   selector: 'app-project-show',
   templateUrl: './project-show.component.html',
@@ -7,9 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectShowComponent implements OnInit {
 
-  constructor() { }
+  project_id = null;
+
+  constructor(
+  	private route: ActivatedRoute,
+  	private router: Router
+  ) { }
 
   ngOnInit(): void {
+  	this.project_id = this.route.snapshot.paramMap.get("id");
   }
 
 }
