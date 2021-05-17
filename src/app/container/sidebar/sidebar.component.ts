8d290ea3e9271483248d04632868ced6800bccdf
Shek Muktar <im.skm07@gmail.com>
2021-05-17 20:56:57 +0530

    sidebar: component: Logged in user name is shown in sidebar

--- src/app/container/sidebar/sidebar.component.ts
+++ src/app/container/sidebar/sidebar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../../services/auth.service';
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  user: any;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+  	this.user = this.authService.getUser();
+  }
+
+  userFullName() {
+  	return this.user.first_name + " " + this.user.last_name;
   }
 
 }
